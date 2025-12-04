758d40f1326f7419df127e21d76d33e50dcfbbaf
Kamil Pawlik <pl.kamilpawlik@gmail.com>
2025-12-04 20:51:18 +0100

    solved: 1689 (extra problem)

---
+++ 1689/main.ts
@@ -0,0 +1,45 @@
+function minPartitions(n: string): number {
+	let maxDigit = 0;
+
+	for (let i = 0; i < n.length; i++) {
+		if (maxDigit < n.charCodeAt(i) - 48) {
+			maxDigit = n.charCodeAt(i) - 48;
+
+			if (maxDigit === 9) break;
+		}
+	}
+
+	return maxDigit;
+}
+
+/*   *   *   *   *   *   *   *   *   *   */
+/*   *   *   *   *   *   *   *   *   *   */
+
+import { assertEquals } from "@std/assert";
+
+Deno.test("Case 1", () => {
+	const n = "32";
+
+	const $result = minPartitions(n);
+	const $expect = 3;
+
+	assertEquals($result, $expect);
+});
+
+Deno.test("Case 2", () => {
+	const n = "82734";
+
+	const $result = minPartitions(n);
+	const $expect = 8;
+
+	assertEquals($result, $expect);
+});
+
+Deno.test("Case 3", () => {
+	const n = "27346209830709182346";
+
+	const $result = minPartitions(n);
+	const $expect = 9;
+
+	assertEquals($result, $expect);
+});
