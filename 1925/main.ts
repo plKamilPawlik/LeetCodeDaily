220c83544b285c9b82b58cd52f3d73a6ffe5f220
Kamil Pawlik <pl.kamilpawlik@gmail.com>
2025-12-08 12:59:53 +0100

    solved: 1925 (optimized)

--- 1925/main.ts
+++ 1925/main.ts
@@ -1,10 +1,18 @@
 function countTriples(n: number): number {
+	const c2max = n ** 2;
 	let triples = 0;
 
 	for (let a = 1; a <= n; a++) {
 		for (let b = 1; b <= n; b++) {
-			for (let c = 1; c <= n; c++) {
-				if (a ** 2 + b ** 2 === c ** 2) triples++;
+			const a2 = a ** 2;
+			const b2 = b ** 2;
+
+			if (a2 + b2 > c2max) continue;
+
+			const sqrt = Math.sqrt(a2 + b2);
+
+			if (Number.isInteger(sqrt)) {
+				triples++;
 			}
 		}
 	}
@@ -28,6 +36,15 @@ Deno.test("Case 1", () => {
 
 Deno.test("Case 2", () => {
 	const n = 10;
+
+	const $result = countTriples(n);
+	const $expect = 4;
+
+	assertEquals($result, $expect);
+});
+
+Deno.test("Case 3", () => {
+	const n = 12;
 
 	const $result = countTriples(n);
 	const $expect = 4;
