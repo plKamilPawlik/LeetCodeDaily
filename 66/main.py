32f21b65d3e30e3058bcb9e821054f99d27a634e
Kamil Pawlik <pl.kamilpawlik@gmail.com>
2026-01-05 15:37:58 +0100

    solved: 66 (with Python)

---
+++ 66/main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+
+        digits[n - 1] += 1
+
+        for i in range(n - 1, 0, -1):
+            if digits[i] >= 10:
+                digits[i] %= 10
+                digits[i - 1] += 1
+            else:
+                break
+
+        if digits[0] >= 10:
+            digits[0] %= 10
+            digits = [1] + digits
+
+        return digits
