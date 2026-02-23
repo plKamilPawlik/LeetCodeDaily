34e5d67af133c7b46cd56cb6c13b5da0f4b51db3
Kamil Pawlik <pl.kamilpawlik@gmail.com>
2026-02-23 12:57:05 +0100

    solved: 762 (optimized)

--- 762/main.ts
+++ 762/main.ts
@@ -1,17 +1,7 @@
+const PRIMES = [2, 3, 5, 7, 11, 13, 17, 19];
+
 function countPrimeSetBits(left: number, right: number): number {
-	const isPrime = (n: number): boolean => {
-		if (n < 2) return false;
-
-		const sqrt = Math.sqrt(n);
-
-		for (let i = 2; i <= sqrt; i++) {
-			if (n % i === 0) return false;
-		}
-
-		return true;
-	};
-
-	const numBits = (n: number): number => {
+	const bitCount = (n: number): number => {
 		let count = n & 1;
 
 		while ((n >>= 1)) if (n & 1) count++;
@@ -21,9 +11,8 @@ function countPrimeSetBits(left: number, right: number): number {
 
 	let ans = 0;
 
-	for (let int = left; int <= right; int++) {
-		const bits = numBits(int);
-		if (isPrime(bits)) ans++;
+	for (let n = left; n <= right; n++) {
+		if (PRIMES.includes(bitCount(n))) ans++;
 	}
 
 	return ans;
