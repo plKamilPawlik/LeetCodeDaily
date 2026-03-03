5f24edc288e3822ebba20d44c57346eb5156876e
Kamil Pawlik <pl.kamilpawlik@gmail.com>
2026-03-03 15:48:22 +0100

    solved: 1545 (alternative)

--- 1545/main.ts
+++ 1545/main.ts
@@ -1,17 +1,16 @@
 function findKthBit(n: number, k: number): string {
-	const build = (s: number): string => {
-		if (s <= 0) return "";
-		if (s === 1) return "0";
+	const prefix = new Array(n + 1);
+	prefix[0] = "";
+	prefix[1] = "0";
 
-		const str = build(s - 1);
+	const inverse = (bits: string[]): string[] => bits.map((c) => c === "1" ? "0" : "1");
+	const reverse = (bits: string[]): string[] => bits.reverse();
 
-		const inv = [...str].map((c) => c === "1" ? "0" : "1");
-		const rev = inv.reverse().join("");
+	for (let i = 2; i <= n; i++) {
+		prefix[i] = prefix[i - 1] + "1" + reverse(inverse([...prefix[i - 1]])).join("");
+	}
 
-		return str + "1" + rev;
-	};
-
-	return build(n)[k - 1];
+	return prefix[n][k - 1];
 }
 
 /*   *   *   *   *   *   *   *   *   *   */
