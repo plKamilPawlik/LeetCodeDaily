ac532e80088013779ce231cdeddc3ee3559d9db3
Kamil Pawlik <pl.kamilpawlik@gmail.com>
2026-03-06 16:32:45 +0100

    solved: 1784 (with Python & optimized)

--- 1784/main.py
+++ 1784/main.py
@@ -1,9 +1,3 @@
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
-        zeros = 0
-
-        for i in range(len(s) - 1):
-            if s[i] == "0" and s[i + 1] == "1":
-                return False
-
-        return True
+        return "01" not in s
